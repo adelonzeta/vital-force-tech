19629905082d2ef9d646bbd202e44467d20dfae8
adelonzeta <adelonzeta@gmail.com>
2016-02-29 11:32:23 -0800

    new contents and readmore implementation

--- beverages/js/scripts.js
+++ beverages/js/scripts.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("main article a").click(function(){$(this).siblings(".hidden").removeClass("hidden"),$(this).hide()})});
+$(document).ready(function(){$("main article .readmore").click(function(){$(this).siblings(".readmore-container").css("height","auto"),$(this).addClass("hidden"),$(this).siblings(".readless").removeClass("hidden")}),$("main article .readless").click(function(){$(this).siblings(".readmore-container").css("height","197px"),$(this).addClass("hidden"),$(this).siblings(".readmore").removeClass("hidden")});window.location.href});
